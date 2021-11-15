d01795fa66282cfaee3651051764388c3a99675b
smudesign <urwinsamantha@gmail.com>
2021-11-14 21:11:59 -0500

    questions

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,7 @@
+DROP TABLE IF EXISTS departments;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS employees;
+
 CREATE TABLE departments (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(50) NOT NULL
@@ -7,13 +11,16 @@ CREATE TABLE roles (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(50) NOT NULL,
     salary INTEGER NOT NULL,
-    CONSTRAINT fk_department FOREIGN KEY department_id REFERENCES departments(id)
+    department_id INTEGER NOT NULL,
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES departments(id)
 );
 
 CREATE TABLE employees (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
-    CONSTRAINT fk_role FOREIGN KEY role_id REFERENCES roles(id),
-    manager_id INTEGER NULL FOREIGN KEY REFERENCES employees(id)
+    role_id INTEGER NOT NULL,
+    manager_id INTEGER,
+    FOREIGN KEY (role_id) REFERENCES roles(id),
+    FOREIGN KEY (manager_id) REFERENCES employees(id)
 );
--- index.js
+++ index.js
@@ -1,1 +1,164 @@
+const inquirer = require('inquirer');
 const db = require('./db/connection');
+
+const start = () => {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'action',
+            message: 'What would you like to do?',
+            choices: ['View All Employees', 'View Employees by Department', 'View Employees by Manager', 'Add Employee', 'Update Employee Role', 'Update Employee Manager', 'Delete Employee',
+            'View Departments', 'Add Department', 'View Roles', 'Add a Role']
+        },
+        {
+            type: 'input',
+            name: 'first_name',
+            message: 'What is the Employees first name?',
+            when: ({ action }) => {
+                if (action === 'Add Employee') {
+                    return true;
+                } else {
+                    return false;
+                }
+            },
+            validate: nameInput => {
+                if(nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a first name!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'last_name',
+            message: 'What is the Employees last name?',
+            when: ({ first_name }) => {
+                if (first_name) {
+                    return true;
+                } else {
+                    return false;
+                }
+            },
+            validate: nameInput => {
+                if(nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a last name!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'list',
+            name: 'role',
+            message: 'What is this Employees role?',
+            choices: ['Front-end Developer', 'Back-end Developer', 'Lead Engineer', 'UX Designer', 'Graphic Designer', 'Creative Director', 'Marketing Coordinator', 'Marketing Analyst', 'Marketing Director',
+            'Accountant', 'Finanial Analyst', 'Finance Manager '],
+            when: ({ last_name }) => {
+                if (last_name) {
+                    return true;
+                }
+                return false;
+            }
+        },
+        {
+            type: 'list',
+            name: 'manager',
+            choices: ['Ali', 'Samantha', 'Dave', 'Tessa', 'None'],
+            when: ({ role }) => {
+                if (role) {
+                    return true;
+                }
+                return false;
+            }
+        },
+        {
+            type: 'list',
+            name: 'updateEmployee',
+            choices: ['Ali Maqsood', 'Samantha Urwin', 'Dave Chappelle', 'Tessa Thompson', 'Ryan Reynolds', 'Marshall Mathers', 'Katherine Segal', 'Robyn Fenty', 'Regina King', 'Jimmy Yang', 'Idris Alba', 'Blake Lively'],
+            message: 'Which employee would you like to update?',
+            when: ({ action }) => {
+                if (action === 'Update Employee Role' || action === 'Update Employee Manager') {
+                    return true;
+                }
+                return false;
+            }
+        },
+        {
+            type: 'list',
+            name: 'newRole',
+            message: 'What role would you like to update to?',
+            choices: ['Front-end Developer', 'Back-end Developer', 'Lead Engineer', 'UX Designer', 'Graphic Designer', 'Creative Director', 'Marketing Coordinator', 'Marketing Analyst', 'Marketing Director',
+            'Accountant', 'Finanial Analyst', 'Finance Manager '],
+            when: ({ action }) => {
+                if (action === 'Update Employee Role') {
+                    return true;
+                }
+                return false;
+            }
+        },
+        {
+            type: 'list',
+            name: 'newManager',
+            message: 'What manager would you like to update to?',
+            choices: ['Ali', 'Samantha', 'Dave', 'Tessa', 'None'],
+            when: ({ action }) => {
+                if (action === 'Update Employee Manager') {
+                    return true;
+                }
+                return false;
+            }
+        },
+        {
+            type: 'list',
+            name: 'deleteEmployee',
+            choices: ['Ali Maqsood', 'Samantha Urwin', 'Dave Chappelle', 'Tessa Thompson', 'Ryan Reynolds', 'Marshall Mathers', 'Katherine Segal', 'Robyn Fenty', 'Regina King', 'Jimmy Yang', 'Idris Alba', 'Blake Lively'],
+            when: ({ action }) => {
+                if (action === 'Delete Employee') {
+                    return true;
+                }
+                return false;
+            }
+        },
+        {
+            type: 'input',
+            name: 'addDepartment',
+            message: 'What department would you like to add?',
+            when: ({ action }) => {
+                if (action === 'Add Department') {
+                    return true;
+                }
+                return false;
+            },
+            validate: nameInput => {
+                if (!nameInput) {
+                    console.log('Please enter the name of the department');
+                    return false;
+                }
+                return true;
+            }
+        },
+        {
+            type: 'input',
+            name: 'addRole',
+            message: 'What role would you like to add?',
+            when: ({ action }) => {
+                if (action === 'Add a Role') {
+                    return true;
+                }
+                return false;
+            },
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                }
+                return false;
+            }
+        }
+    ])
+    .then(answers => console.log(answers));
+};
+
+start();
